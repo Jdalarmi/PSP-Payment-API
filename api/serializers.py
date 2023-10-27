e51ec5dc4499bf466a0e0b23676185e0af33ff0e
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-10-27 13:54:11 -0300

    Endpoint de login

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 class UserSerializer(serializers.ModelSerializer):
+    def validate(self, value):
+        username = value['username']
+        password = value['password']
+        if username == password:
+            raise serializers.ValidationError('Campos iguais', code=status.HTTP_400_BAD_REQUEST)
+        return value
+    
+    def validate_username(self, attrs):
+        if len(attrs) < 2:
+            raise serializers.ValidationError('username menor que 2 caracteres', code=status.ba)
+        return attrs
+    
+    def validate_password(self, attrs):
+        if len(attrs) < 6:
+            raise serializers.ValidationError('Senha menor que 6 digitos', code=status.HTTP_400_BAD_REQUEST)
+        return attrs
 
     def create(self, validated_data):
-      
         user = User(
             username = validated_data['username'],
             password = validated_data['password']
--- api/views.py
+++ api/views.py
@@ -1,14 +1,21 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from api.serializers import UserSerializer
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 from django.contrib.auth.models import User
 
+
+@swagger_auto_schema(method='post', request_body=UserSerializer)
 @api_view(['POST'])
 def login(request):
     serializer = UserSerializer(data=request.data)
-    
+    username = request.data['username']
+
     if serializer.is_valid():
-        serializer.save()    
-    return Response(serializer.data)
+        serializer.save()
+        return Response(f'Usuario {username} criado com sucesso!!!')
+    else:
+        return Response(serializer.errors)   
+
