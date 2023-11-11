f3e7d03ca11c14a2c5cbf1a30f45847b0de00a1b
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-11-11 13:22:38 -0300

    Update de saldo na conta

--- api/views.py
+++ api/views.py
@@ -1,41 +1,47 @@
-from api.serializers import PayablesSerializer, TransactionSerializer, UserSerializer
-from api.models import Payables, Transaction
+from api.serializers import TransactionSerializer, PayablesSerializer
+from api.models import Transaction, Payables
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 
 
-'''
-@swagger_auto_schema(method='post', request_body=UserSerializer)
-@api_view(['POST'])
-def register_user(request):
-    serializer = UserSerializer(data=request.data)
-    username = request.data['username']
+funds_value  = 150
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(f'Usuario {username} criado com sucesso!!!')
-    else:
-        return Response(serializer.errors)   
-'''
 @swagger_auto_schema(method='post', request_body=TransactionSerializer)
 @api_view(['POST'])
 def register_transaction(request):
+    '''
+        CADA COMPRA LEVA EM CONSIDERAÇÃO QUE VOCÊ TEM R$:150,00 DISPONIVEL DE SALDO.
+        SE VOCÊ PASSAR DESSE VALOR A API BLOQUEIA AS TRANSAÇÕES!
+    '''
     serializer = TransactionSerializer(data=request.data)
     value = request.data['payment_value']
-    method = request.data['payment_method'] 
-    last_four_digits = request.data['card_number'][-4:]
-    masked_card_number = '*' * 15 + last_four_digits
+    method = request.data['payment_method']
+    card_number = request.data['card_number']
+    
+    global funds_value
 
+    if not sufficient_balance(value):
+        return Response({'error': 'Saldo insuficiente'}, status=status.HTTP_400_BAD_REQUEST)
+
+    last_four_digits = card_number[-4:]
+    masked_card_number = '*' * 15 + last_four_digits
     request.data['card_number'] = masked_card_number
 
-
     if serializer.is_valid():
+        funds_value -= value
         Transaction.value_return(value=value, method=method)
         serializer.save()
-        return Response('Transacao realizada com sucesso!!!')
+        return Response(f'Transação realizada com sucesso!!!OBS:SEU SALDO É DE SOMENTE R$:{funds_value}')
     else:
         return Response(serializer.errors)
+
+def sufficient_balance(payment_value):
+    global funds_value
+    return funds_value >= payment_value
+
+
 
 @swagger_auto_schema(method='get')
 @api_view(['GET'])
